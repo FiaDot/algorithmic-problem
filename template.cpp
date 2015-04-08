d444f326a22144c1ed055986dcda0d1344f94ab8
fiadot <fiadot@seed9.com>
2015-04-08 09:34:24 +0900

    [Add] template

---
+++ template.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+	int cases;
+
+	cin >>  cases;
+
+	while(cases--) 
+	{
+		cout << "Hello world!" << endl;
+	}
+
+	return 0;
+}
+
